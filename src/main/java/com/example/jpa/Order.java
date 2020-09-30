cbe071333e0c2d17bfa9fa05d0403e120401944a
winkymonkey <pal.abhishek.2008@gmail.com>
2020-09-30 20:01:01 +0530

    refactor

---
+++ src/main/java/com/example/jpa/Order.java
@@ -0,0 +1,29 @@
+package com.example.jpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "T_ORDER")
+public class Order {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ORDER_ID")
+	private long orderId;
+
+	@Column(name = "TOTAL_PRICE")
+	private double totalPrice;
+	
+	@OneToMany(mappedBy="order")
+	private List<OrderItem> items = new ArrayList<OrderItem>();
+	
+	//Getter, Setter
+}
---
+++ src/main/java/com/example/jpa/OrderItem.java
@@ -0,0 +1,28 @@
+package com.example.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "T_ORDER_ITEM")
+public class OrderItem {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ORDER_ITEM_ID")
+	private long orderItemId;
+
+	@Column(name = "QUANTITY")
+	private int quantity;
+
+	@ManyToOne
+	@JoinColumn(name = "fk_orderId")
+	private Order order;
+	
+	// Getter, Setter
+}
